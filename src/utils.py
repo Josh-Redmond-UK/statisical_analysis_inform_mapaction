3050267d1019463caa3204903f3971ffe4fd2641
Josh Redmond <jr725@exeter.ac.uk>
2024-05-18 15:14:24 +0200

    added arrow plot and flexible component number

--- src/utils.py
+++ src/utils.py
@@ -14,21 +14,29 @@ def parse_data(path='data/INFORM_Risk_2024_v067.xlsx', sheet_name='INFORM Risk 2
 
     return data
 
-def create_pca_plots(data, cols, title):
+def create_pca_plots(data, cols, title, n_components=2):
     inform_risk = data['INFORM RISK']
 
     data = data[cols]
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=n_components)
     pca.fit(data)
-    evr = pca.explained_variance_ratio_
+    evr = pca.explained_variance_ratio_[:2]
     top_1 = np.argmax(evr)
     top_comp_explains = round(evr[top_1], 2)
-    comps = (abs( pca.components_ ))
-    top_1_comp = comps[top_1]
+    comps = pca.components_[0:2]
+
+    arrow_d = np.transpose(comps)
+
+
+
+    top_1_comp = abs(comps)[top_1]
     feat_importance_frame = pd.DataFrame(top_1_comp, index=cols, columns=['Importance'])
     feat_importance_frame.to_csv('outputs/'+title +  " top 1 component explains " f'{top_comp_explains} variance feature importance.csv')
 
     components = np.array(pca.transform(data))
+    components[:,0] *= 1.0/(components[:,0].max() - components[:,0].min())
+    components[:,1] *= 1.0/(components[:,1].max() - components[:,1].min())
+
     fig = plt.figure(figsize = (12,12))
     plt.xlabel('Principal Component 1', fontsize = 15)
     plt.ylabel('Principal Component 2', fontsize = 15)
@@ -36,6 +44,11 @@ def create_pca_plots(data, cols, title):
     sc = plt.scatter(components[:,0], components[:,1], c=inform_risk)
     cbar = plt.colorbar(sc)
     cbar.set_label("INFORM RISK", fontsize = 15)
+
+    for i in range(len(cols)):
+        plt.arrow(0, 0, arrow_d[i,0], arrow_d[i,1], color = 'r', alpha = 0.5, head_width = 0.025, head_length = 0.01)
+        plt.text(arrow_d[i,0]*1.15, arrow_d[i,1]*1.15, cols[i], color = 'g', ha = 'center', va = 'center')
+
     plt.savefig('outputs/'+title + 'PCA.png')
 
 
